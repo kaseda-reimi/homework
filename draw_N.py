66ca8deb4356f971d4e5d6f125cd7d6a24e817bb
kaseda-reimi <84499107+kaseda-reimi@users.noreply.github.com>
2021-05-28 16:36:23 +0900

    Create draw_N.py

---
+++ draw_N.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.linspace(0, 1, 100)
+plt.figure(0)
+plt.title('logistic map')
+plt.plot(x, 4*x*(1-x))
+plt.plot(x, x*(1-x))
+plt.xlabel('x')
+plt.ylabel('y')
+plt.legend(["r=4", "r=1"])
+plt.show()
