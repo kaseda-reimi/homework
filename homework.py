f864844380ad7ab1e77f695f499440eb4991842a
加世田怜美 <kasedareibi@MacBook-Air.local>
2021-06-01 19:27:31 +0900

    homework.py

--- homework.py
+++ homework.py
@@ -1,12 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.linspace(10, 1, 100)
+tsp = 3e-9
+G = 1e-6
+N0 = 2e18
+tph = 1e-12
+b = np.array([[1e-5], [1e-2], [1e-1], [0.5]])
+
+R = np.linspace(25, 29, 100)
+
+Ne = (-tsp*G*R-1/tph+G*(b-1)*N0)/(2*G*(b-1))
+
+
 plt.figure(0)
-plt.title('logistic map')
-plt.plot(x, 4*x*(1-x))
-plt.plot(x, x*(1-x))
-plt.xlabel('x')
-plt.ylabel('y')
-plt.legend(["r=4", "r=1"])
-plt.savefig("Ngraph.png")
+plt.plot(R, Ne[0]))
+plt.plot(R, Ne[1]))
+plt.xlabel('log10(R)')
+plt.ylabel('Ne')
+plt.legend(["β=1e-5", "β=1e-2"])
+plt.savefig("Ne.png")
