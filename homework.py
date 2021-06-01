bd1bb077338024f358680b5e084c4f54f327ab6f
kaseda-reimi <84499107+kaseda-reimi@users.noreply.github.com>
2021-06-01 18:50:07 +0900

    Update homework.py

--- homework.py
+++ homework.py
@@ -1,2 +1,12 @@
 import numpy
 import matplotlib.pyplot as plt
+
+x = np.linspace(10, 1, 100)
+plt.figure(0)
+plt.title('logistic map')
+plt.plot(x, 4*x*(1-x))
+plt.plot(x, x*(1-x))
+plt.xlabel('x')
+plt.ylabel('y')
+plt.legend(["r=4", "r=1"])
+plt.savefig("Ngraph.png")
