05df883f87fd08a3a417ac20eb9759cb9866f553
加世田怜美 <kasedareibi@MacBook-Air.local>
2021-06-01 21:34:57 +0900

    homework.py

--- homework.py
+++ homework.py
@@ -13,6 +13,7 @@ Z = -tsp*G*R-1/tph+G*(b-1)*N0
 Ne =(Z-np.sqrt(Z**2+4*tsp/tph*G*R*b))/(2*G*(b-1))
 logNe = np.log10(Ne)
 
+Np = tph*R+tph/tsp*(b-1)*Ne
 
 plt.figure(0)
 plt.plot(logR, logNe[0])
@@ -23,3 +24,13 @@ plt.xlabel('log R)')
 plt.ylabel('log Ne')
 plt.legend(["β=1e-5", "β=1e-2", "β=1e-1", "β=0.5"])
 plt.savefig("Ne.png")
+
+plt.figure(1)
+plt.plot(logR, logNp[0])
+plt.plot(logR, logNp[1])
+plt.plot(logR, logNp[2])
+plt.plot(logR, logNp[3])
+plt.xlabel('log R)')
+plt.ylabel('log Np')
+plt.legend(["β=1e-5", "β=1e-2", "β=1e-1", "β=0.5"])
+plt.savefig("Np.png")
