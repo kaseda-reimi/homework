53e34dacf3175a6464fdff47296b75110f1c2b74
加世田怜美 <kasedareibi@MacBook-Air.local>
2021-06-01 19:41:38 +0900

    homework.py

--- homework.py
+++ homework.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 
 tsp = 3e-9
 G = 1e-6
@@ -7,9 +8,10 @@ N0 = 2e18
 tph = 1e-12
 b = np.array([[1e-5], [1e-2], [1e-1], [0.5]])
 
-R = np.linspace(25, 29, 100)
-
+logR = np.linspace(25, 29, 100)
+R = 10**logR
 Ne = (-tsp*G*R-1/tph+G*(b-1)*N0)/(2*G*(b-1))
+logNe = math.log10(Ne)
 
 
 plt.figure(0)
