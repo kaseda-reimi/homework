67d8e7c576e5660b11fb1eb7134bb08827deb379
加世田怜美 <kasedareibi@MacBook-Air.local>
2021-06-01 20:51:49 +0900

    homework.py

--- homework.py
+++ homework.py
@@ -1,6 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import math
 
 tsp = 3e-9
 G = 1e-6
@@ -10,14 +9,15 @@ b = np.array([[1e-5], [1e-2], [1e-1], [0.5]])
 
 logR = np.linspace(25, 29, 100)
 R = 10**logR
+logR = np.log(R)
 Ne = (-tsp*G*R-1/tph+G*(b-1)*N0)/(2*G*(b-1))
-logNe = math.log10(Ne)
+logNe = np.log(Ne)
 
 
 plt.figure(0)
-plt.plot(R, Ne[0])
-plt.plot(R, Ne[1])
-plt.xlabel('log10(R)')
-plt.ylabel('Ne')
+plt.plot(logR, logNe[0])
+plt.plot(logR, logNe[1])
+plt.xlabel('log R)')
+plt.ylabel('log Ne')
 plt.legend(["β=1e-5", "β=1e-2"])
 plt.savefig("Ne.png")
