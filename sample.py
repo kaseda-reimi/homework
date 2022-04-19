4243fe7b459266cf0ac65b2facb5f980df4940c9
kaseda-reimi <kaseda-reim-xc@ynu.jp>
2022-04-19 19:17:09 +0900

    sample.py

---
+++ sample.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+print('Hello World!')
+a = 2
+b = 3
+print(a+b)
+
+try:
+    n = 1000
+    x = np.linspace(0, 30, n)
+    y = np.sin(x)*np.exp(-x/10)
+    plt.plot(x, y)
+    plt.title('Sample Output')
+    plt.show()
+    print('Accepted')
+
+except:
+    print('Error : Some Module are not installed!')
